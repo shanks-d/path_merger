33b937af77d28be328acf639f3a9197a01246958
shanks-d <shshnk.sd@gmail.com>
2023-09-13 21:24:15 +0200

    Added ROS 2 message to describe paths

---
+++ src/placeholder.cpp
@@ -0,0 +1,9 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "path_merger/msg/path.hpp"
+
+int main(int argc, char * argv[])
+{
+  return 0;
+}
