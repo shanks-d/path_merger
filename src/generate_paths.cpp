eaac7aa1116124c813194560b91f5f141993306b
shanks-d <shshnk.sd@gmail.com>
2023-09-14 18:46:08 +0200

    Added input path data generator node

---
+++ src/generate_paths.cpp
@@ -0,0 +1,101 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <stdexcept> 
+
+#include "rclcpp/rclcpp.hpp"
+
+#define LANE_CHANGE 1
+
+std::vector<float> path1_x, path1_y;
+std::vector<float> path2_x, path2_y;
+
+const float resolution = 0.2;   // meters per pixel
+
+void laneChange(){
+    // path1
+    // straight segment 
+    float y = -1.0;
+    float x = 0.0;
+    float x1 = -9.0, x2 = 2.0;
+    int size = (x2 - x1)/resolution;
+    for(int i = 0; i <= size; i++){
+        x = x1 + i*resolution;
+        path1_x.push_back(x);
+        path1_y.push_back(y);
+    }
+    
+    // path2
+    // straight segment
+    y = -1.0;
+    x1 = -6.0;
+    x2 = -5.0;
+    size = (x2 - x1)/resolution;
+    for(int i = 0; i < size; i++){
+        x = x1 + i*resolution;
+        path2_x.push_back(x);
+        path2_y.push_back(y);
+    }
+    // curved segment
+    x1 = -5.0;
+    x2 = 5.0;
+    size = (x2 - x1)/resolution;
+    for(int i = 0; i <= size; i++){
+        x = x1 + i*resolution;
+        y = sin(0.1*M_PI*x);
+        path2_x.push_back(x);
+        path2_y.push_back(y);
+    }
+
+    // check if paths are of equal lengths 
+    if(path1_x.size() != path2_x.size()){
+        throw std::runtime_error("Unequal path lengths");
+    }
+}
+
+// compute waypoints for different scenarios
+void computePoints(int scenario){
+    switch(scenario){
+        case 1:
+            laneChange();
+            break;
+        default:
+            throw std::runtime_error("Invalid scenario");
+            break;
+    }
+}
+
+void writePathData(std::string filename){
+    // create an output filestream object
+    std::ofstream pathFile(filename);
+    
+    // send column names to the stream
+    pathFile << "x1,y1,x2,y2" << std::endl;
+    
+    // send data to the stream
+    int length = path1_x.size();
+    for(int i = 0; i < length; i++){
+        pathFile << path1_x[i] << "," << path1_y[i] << "," << path2_x[i] << "," << path2_y[i] << std::endl;
+    }
+    
+    // close the file
+    pathFile.close();
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+
+    // scenarios: LANE_CHANGE
+    computePoints(LANE_CHANGE);
+    
+    // write the vector to CSV
+    writePathData("scenario1.csv");
+
+    rclcpp::shutdown();
+    return 0;
+}
--- src/input_paths.cpp
+++ src/input_paths.cpp
@@ -3,6 +3,9 @@
 #include <memory>
 #include <string>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/point.hpp"
@@ -12,15 +15,23 @@ using Path = path_merger::msg::Path;
 using Point = geometry_msgs::msg::Point;
 using namespace std::chrono_literals;
 
-const std::vector<std::pair<float, float>> p1 {{1,0}, {2,0}, {3,0}, {4,0}, {5,0}, {6,0}, {7,0}, {8,0}, {9,0}};
-const std::vector<std::pair<float, float>> p2 {{5,-0.5}, {6,-1.5}, {7,-2}, {8,-3}, {9,-5}};
+// system path to the path_merger package
+#define packageDir "/home/shanks/ros2_ws/src/path_merger"
+
+// const std::vector<std::pair<float, float>> p1 {{1,0}, {2,0}, {3,0}, {4,0}, {5,0}, {6,0}, {7,0}, {8,0}, {9,0}};
+// const std::vector<std::pair<float, float>> p2 {{5,-0.5}, {6,-1.5}, {7,-2}, {8,-3}, {9,-5}};
+
+const std::unordered_map<int, std::string> scenarioNames {{1, "Lane Change"}};
 
 class PathPublisher : public rclcpp::Node
 {
   public:
     PathPublisher()
     : Node("input_paths_publisher"){
-        initializePaths();
+        this->declare_parameter("test_scenario", 1);    //default scenario 1
+
+        // initializePaths();
+        readPathData();
 
         pub1_ = this->create_publisher<Path>("path1", 10);
         timer1_ = this->create_wall_timer(1s, std::bind(&PathPublisher::pub1Callback, this));
@@ -30,28 +41,101 @@ class PathPublisher : public rclcpp::Node
     }
 
   private:
-    void initializePaths(){
-        // Path1
+    void readPathData(){
+        
+        int scenario = this->get_parameter("test_scenario").as_int();
+        auto itr = scenarioNames.find(scenario);
+        if(itr == scenarioNames.end()){
+            throw std::runtime_error("Invalid scenario number");
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Scenario %d selected: %s!", scenario, itr->second.c_str());
+        std::string filename(packageDir);
+        filename.append("/data/scenario" + std::to_string(scenario) + ".csv");
+
+        // create an input filestream
+        std::ifstream pathFile(filename);
+
+        // make sure the file is open
+        if(!pathFile.is_open()){
+            throw std::runtime_error("Could not open file");
+        }
+
+        std::string line;
+
+        // read and ignore the column names 
+        std::getline(pathFile, line);
+
+        // read data, line by line
+        while(std::getline(pathFile, line)){
+            // create a stringstream of the current line
+            std::stringstream ss(line);
+            
+            // keep track of the index to populate the correct path
+            int idx = 0;
+            float val;
+            Point p1, p2;
+            
+            // extract each integer
+            while(ss >> val){                
+                if(idx == 0){
+                    p1.x = val;
+                }
+                else if(idx == 1){
+                    p1.y = val;
+                }
+                else if(idx == 2){
+                    p2.x = val;
+                }
+                else{
+                    p2.y = val;
+                }
+                
+                // ignore comma and move on
+                if(ss.peek() == ',') ss.ignore();
+                
+                // increment the index
+                idx++;
+            }
+
+            path1_.points.push_back(p1);
+            path2_.points.push_back(p2);
+        }
+
+        // close file
+        pathFile.close();
+
+        // path headers
         rclcpp::Time now = this->get_clock()->now();
         path1_.header.stamp = now - rclcpp::Duration(500000000);  //time gap of 500ms
         path1_.header.frame_id = "path1";
-        for(auto p: p1){
-            Point point;
-            point.x = p.first;
-            point.y = p.second;
-            path1_.points.push_back(point);
-        }
-
-        // Path2
         path2_.header.stamp = now;
         path2_.header.frame_id = "path2";
-        for(auto p: p2){
-            Point point;
-            point.x = p.first;
-            point.y = p.second;
-            path2_.points.push_back(point);
-        }
     }
+
+    // void initializePaths(){
+    //     // Path1
+    //     rclcpp::Time now = this->get_clock()->now();
+    //     path1_.header.stamp = now - rclcpp::Duration(500000000);  //time gap of 500ms
+    //     path1_.header.frame_id = "path1";
+    //     for(auto p: p1){
+    //         Point point;
+    //         point.x = p.first;
+    //         point.y = p.second;
+    //         path1_.points.push_back(point);
+    //     }
+
+    //     // Path2
+    //     path2_.header.stamp = now;
+    //     path2_.header.frame_id = "path2";
+    //     for(auto p: p2){
+    //         Point point;
+    //         point.x = p.first;
+    //         point.y = p.second;
+    //         path2_.points.push_back(point);
+    //     }
+    // }
+
     void pub1Callback(){
         pub1_->publish(path1_);
         RCLCPP_INFO_ONCE(this->get_logger(), "Publishing input path1");
